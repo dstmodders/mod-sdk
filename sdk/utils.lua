4a111dca923e93d0655f5b48a037a75d1d8d9e1a
Victor Popkov <victor@popkov.me>
2020-10-10 15:01:50 +0300

    Add Utils

---
+++ sdk/utils.lua
@@ -0,0 +1,40 @@
+----
+-- Different mod utilities.
+--
+-- Includes different utilities used throughout the whole mod.
+--
+-- **Source Code:** [https://github.com/victorpopkov/dst-mod-sdk](https://github.com/victorpopkov/dst-mod-sdk)
+--
+-- @module SDK.Utils
+-- @see SDK.Utils.Chain
+-- @see SDK.Utils.Methods
+-- @see SDK.Utils.String
+-- @see SDK.Utils.Table
+--
+-- @author Victor Popkov
+-- @copyright 2020
+-- @license MIT
+-- @release 0.1
+----
+local Utils = {}
+
+Utils.Chain = require "sdk/utils/chain"
+Utils.Methods = require "sdk/utils/methods"
+Utils.String = require "sdk/utils/string"
+Utils.Table = require "sdk/utils/table"
+
+--- Asserts if the required field is not missing.
+-- @tparam string name
+-- @tparam any field
+function Utils.AssertRequiredField(name, field)
+    assert(field ~= nil, string.format("Required %s is missing", name))
+end
+
+--- Checks if value is an integer.
+-- @tparam number value
+-- @treturn boolean
+function Utils.IsInteger(value)
+    return value == math.floor(value)
+end
+
+return Utils
