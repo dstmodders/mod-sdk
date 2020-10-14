1fa9f69669bebefb2694797b8f776bbd6fa91f2e
Victor Popkov <victor@popkov.me>
2020-10-14 16:31:18 +0300

    Add helper spec

---
+++ spec/helper.lua
@@ -0,0 +1,54 @@
+--
+-- Packages
+--
+
+local preloads = {
+    class = "spec/class",
+}
+
+package.path = "./sdk/?.lua;" .. package.path
+for k, v in pairs(preloads) do
+    package.preload[k] = function()
+        return require(v)
+    end
+end
+
+--
+-- General
+--
+
+function Empty()
+end
+
+function ReturnValueFn(value)
+    return function()
+        return value
+    end
+end
+
+--
+-- Asserts
+--
+
+function AssertChainNil(fn, src, ...)
+    if src and (type(src) == "table" or type(src) == "userdata") then
+        local args = { ... }
+        local start = src
+        local previous, key
+
+        for i = 1, #args do
+            if start[args[i]] then
+                previous = start
+                key = args[i]
+                start = start[key]
+            end
+        end
+
+        if previous and src then
+            previous[key] = nil
+            args[#args] = nil
+            fn()
+            AssertChainNil(fn, src, unpack(args))
+        end
+    end
+end
