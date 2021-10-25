eaea60ec9f53b91a72bb47443bde454a4b8f4a20
Victor Popkov <victor@popkov.me>
2021-10-25 17:30:35 +0300

    Add options support in SDK.Input.AddConfigKey...
    Closes #4.
    
    Supported options:
    
    - ignore_console_screen
    - ignore_has_input_focus
    - ignore_map_screen
    - ignore_screens

--- sdk/input.lua
+++ sdk/input.lua
@@ -18,9 +18,70 @@ local SDK
 --- Helpers
 -- @section helpers
 
+local function DebugErrorFn(...)
+    SDK._DebugErrorFn(Input, ...)
+end
+
+local function DebugErrorKey(fn_name, key, ...)
+    DebugErrorFn(fn_name, "[" .. key .. "]", ...)
+end
+
+local function DebugErrorOptions(fn_name, key, msg)
+    if msg ~= nil then
+        DebugErrorKey(fn_name, key, "Invalid options passed (" .. msg .. ")")
+        return
+    end
+    DebugErrorKey(fn_name, key, "Invalid options passed")
+end
+
 local function GetKeyFromConfig(config)
     local key = GetModConfigData(config, SDK.modname)
     return key and (type(key) == "number" and key or _G[key]) or -1
+end
+
+local function PrepareOptions(fn_name, key, options)
+    if type(options) ~= "table" then
+        DebugErrorOptions(fn_name, key, "must be a table")
+        return false
+    end
+
+    if options.ignore_screens ~= nil and type(options.ignore_screens) ~= "table" then
+        DebugErrorOptions(fn_name, key, "ignore_screens must be a table")
+        return false
+    elseif options.ignore_screens == nil then
+        options.ignore_screens = {}
+    end
+
+    if options.ignore_console_screen ~= nil
+        and type(options.ignore_console_screen) ~= "boolean"
+    then
+        DebugErrorOptions(fn_name, key, "ignore_console_screen must be a boolean")
+        return false
+    end
+
+    if options.ignore_map_screen ~= nil
+        and type(options.ignore_map_screen) ~= "boolean"
+    then
+        DebugErrorOptions(fn_name, key, "ignore_map_screen must be a boolean")
+        return false
+    end
+
+    if options.ignore_has_input_focus ~= nil
+        and type(options.ignore_has_input_focus) ~= "boolean"
+    then
+        DebugErrorOptions(fn_name, key, "ignore_has_input_focus must be a boolean")
+        return false
+    end
+
+    if options.ignore_console_screen == true then
+        table.insert(options.ignore_screens, "ConsoleScreen")
+    end
+
+    if options.ignore_map_screen == true then
+        table.insert(options.ignore_screens, "MapScreen")
+    end
+
+    return options
 end
 
 --- General
@@ -36,36 +97,54 @@ function Input.IsControlMove(control)
         or control == CONTROL_MOVE_RIGHT
 end
 
---- Adds a config key down handler.
--- @tparam string config
--- @tparam function fn
--- @tparam[opt] boolean can_handle_key
-function Input.AddConfigKeyDownHandler(config, fn, can_handle_key)
-    local config_key = GetKeyFromConfig(config)
-    if fn and config_key then
-        TheInput:AddKeyDownHandler(config_key, can_handle_key ~= nil and function()
-            if SDK.FrontEnd.CanHandleKey() then
-                return fn()
-            end
-        end or fn)
-    end
-end
-
 --- Adds a config key handler.
 -- @tparam string config
 -- @tparam function fn
--- @tparam[opt] boolean can_handle_key
-function Input.AddConfigKeyHandler(config, fn, can_handle_key)
+-- @tparam[opt] table options
+function Input.AddConfigKeyHandler(config, fn, options)
     local config_key = GetKeyFromConfig(config)
     if fn and config_key then
+        local fn_name = "AddConfigKeyHandler"
+        options = PrepareOptions(fn_name, config, options)
         TheInput:AddKeyHandler(function(key, down)
             if key == config_key then
-                if can_handle_key ~= nil and SDK.FrontEnd.CanHandleKey() then
-                    return fn(down)
-                else
-                    fn(down)
+                if options.ignore_has_input_focus ~= true and SDK.FrontEnd.HasInputFocus() then
+                    return
+                end
+
+                for _, screen in pairs(options.ignore_screens) do
+                    if SDK.FrontEnd.IsScreenOpen(screen) then
+                        return
+                    end
+                end
+
+                fn(down)
+            end
+        end)
+    end
+end
+
+--- Adds a config key down handler.
+-- @tparam string config
+-- @tparam function fn
+-- @tparam[opt] table options
+function Input.AddConfigKeyDownHandler(config, fn, options)
+    local config_key = GetKeyFromConfig(config)
+    if fn and config_key then
+        local fn_name = "AddConfigKeyDownHandler"
+        options = PrepareOptions(fn_name, config, options)
+        TheInput:AddKeyDownHandler(config_key, function()
+            if options.ignore_has_input_focus ~= true and SDK.FrontEnd.HasInputFocus() then
+                return
+            end
+
+            for _, screen in pairs(options.ignore_screens) do
+                if SDK.FrontEnd.IsScreenOpen(screen) then
+                    return
                 end
             end
+
+            return fn()
         end)
     end
 end
@@ -73,15 +152,25 @@ end
 --- Adds a config key up handler.
 -- @tparam string config
 -- @tparam function fn
--- @tparam[opt] boolean can_handle_key
-function Input.AddConfigKeyUpHandler(config, fn, can_handle_key)
+-- @tparam[opt] table options
+function Input.AddConfigKeyUpHandler(config, fn, options)
     local config_key = GetKeyFromConfig(config)
     if fn and config_key then
-        TheInput:AddKeyUpHandler(config_key, can_handle_key ~= nil and function()
-            if SDK.FrontEnd.CanHandleKey() then
-                return fn()
+        local fn_name = "AddConfigKeyUpHandler"
+        options = PrepareOptions(fn_name, config, options)
+        TheInput:AddKeyUpHandler(config_key, function()
+            if options.ignore_has_input_focus ~= true and SDK.FrontEnd.HasInputFocus() then
+                return
             end
-        end or fn)
+
+            for _, screen in pairs(options.ignore_screens) do
+                if SDK.FrontEnd.IsScreenOpen(screen) then
+                    return
+                end
+            end
+
+            return fn()
+        end)
     end
 end
 
